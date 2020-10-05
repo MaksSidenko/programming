8298105817d3946fe9de7c65eefce522f913c8ce
maks <maks.sidenko.02@gmail.com>
2020-10-06 01:14:50 +0300

    I added a ready-made part of the full screen

--- first_31_08_2020/dist/js/script.js
+++ first_31_08_2020/dist/js/script.js
@@ -27,5 +27,10 @@ testWebP(function (support) {
    }
 });
 $('.menu__burger').click(function (event) {
-   $(this).toggleClass("burger__active")
+   $(this).toggleClass("burger__active");
+   $(".menu__nav").toggleClass("menu__nav-active");
+   $(".body").toggleClass("body-767");
+   $(".logo-width-1024-block").toggleClass("logo-width-1024-block-hide");
+   $(".menu__logo").toggleClass("menu__logo-active");
+   $(".body").toggleClass("body-active");
 });
